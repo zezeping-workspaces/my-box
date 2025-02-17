2a792c9eb3bb904022eb61e447df6bbac0b6e7bb
sanmu <578595193@qq.com>
2025-02-17 18:00:32 +0800

    FEAT: convert css oklch to rgb

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,14 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import tailwindcss from '@tailwindcss/vite'
+import chroma from 'chroma-js';
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [tailwindcss(), vue(), vueJsx()],
+  plugins: [oklchToRgbPlugin(), tailwindcss(), vue(), vueJsx()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
@@ -37,3 +38,27 @@ export default defineConfig(async () => ({
     },
   }
 }));
+
+
+function oklchToRgbPlugin() {
+  return {
+    name: 'vite-plugin-oklch-to-rgb',
+    transform(code: string, id: string) {
+      if (/\.css$/.test(id)) {
+        const regex = /oklch\(([^)]+)\)/g;
+        let matches = null;
+        while ((matches = regex.exec(code)) !== null) {
+          const oklchValue = matches[0];
+          const [l, c, h] = matches[1].split(' ').map(Number);
+          const rgb = chroma.oklch(l, c, h).rgb(); // 将 oklch 转换为 rgb
+          const rgbString = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
+          code = code.replace(oklchValue, rgbString);
+        }
+      }
+      return {
+        code,
+        map: null,
+      };
+    },
+  };
+}
